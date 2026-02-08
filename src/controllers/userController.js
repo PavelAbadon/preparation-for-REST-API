f9b2239830ecdbee9f3e017a056f3dee9890f390
Pavel Vasilev <pavelabadon@gmail.com>
2026-02-08 15:18:12 +0200

    Create first user without verification

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
+import { userService } from "../services/index.js";
 
 const userController = Router();
 
 userController.get('/register', (req, res) => {
     res.render('users/register', );
 });
+
+userController.post('/register', async(req, res) =>{
+    const {email, password, rePassword} = req.body;
+    const result = await userService.register( email, password, rePassword );
+
+    res.end();
+})
 
 
 export default userController;
