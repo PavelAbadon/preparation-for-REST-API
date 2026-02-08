c3b741e263b9083dc9cd3ca1406c033037082a6e
Pavel Vasilev <pavelabadon@gmail.com>
2026-02-08 11:37:20 +0200

    add ErrorController

---
+++ src/controllers/errorController.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const errorController = Router();
+
+export default errorController;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import homeController from "./controllers/homeController.js";
+import errorController from "./controllers/errorController.js";
 
 const routes = Router();
 routes.use(homeController);
+routes.use(errorController)
 
 
 export default routes
