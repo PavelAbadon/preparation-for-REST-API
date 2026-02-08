909cc28756e9599deec730ee6e16711156e107ee
Pavel Vasilev <pavelabadon@gmail.com>
2026-02-08 13:10:44 +0200

    add handlebars

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import routes from './routes.js';
+import handlebars from 'express-handlebars';
 
 const app = express();
+
+//Install handlebars
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', '/src/views');
 
 // Add static middleware
 app.use(express.static('src/public'));
 // Add body parser
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:false}));
 // Add routes
-app.use(routes)
+app.use(routes);
 
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000.......'))
