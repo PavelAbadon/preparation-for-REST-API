7938a6f48edefac763876a38841c519331904c97
Pavel Vasilev <pavelabadon@gmail.com>
2026-02-08 14:18:51 +0200

    add register view

---
+++ src/controllers/userController.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const userController = Router();
+
+userController.get('/register', (req, res) => {
+    res.render('users/register', );
+});
+
+
+export default userController;
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import homeController from "./controllers/homeController.js";
 import errorController from "./controllers/errorController.js";
+import userController from "./controllers/userController.js";
 
 const routes = Router();
 routes.use(homeController);
-
+routes.use('/users', userController);
 
 
 //Last Controller is ErrorController
