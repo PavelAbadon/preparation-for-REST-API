8a376565d9d97e50396421ddd9a1b95da6e3d5a0
Pavel Vasilev <pavelabadon@gmail.com>
2026-02-16 16:22:21 +0200

    dynamic Home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import * as animalService from "../services/animalService.js";
 
 const homeController = Router();
 
-homeController.get('/', (req, res) => {
-  res.render('home', {pageTitle: 'Home page'});
+homeController.get('/', async(req, res) => {
+  const lastestAnimals = await animalService.getLatest();
+  console.log(lastestAnimals);
+
+
+  res.render('home', {animals:lastestAnimals, pageTitle: 'Home page'});
 });
 
 export default homeController;
--- src/services/animalService.js
+++ src/services/animalService.js
@@ -4,6 +4,10 @@ export function getAllAnimals (){
     return Animal.find();
 }
 
+export function getLatest (){
+    return Animal.find().sort({_id:-1}).limit(3);
+}
+
 export function createAnimal(animalData, userId) {
     return Animal.create({
         ...animalData, 
